5de90d302e3ad04fcf72b59de93b2ea2a62640fe
Shishir-sap <shishirsapkota@msn.com>
2022-12-08 13:35:24 +0000

    aport change for deployment

--- flask_app.py
+++ flask_app.py
@@ -10,8 +10,7 @@ microweb_app = Flask(__name__)
 def main():
     return render_template("index.html")
 
-
 if __name__ == "__main__":
     microweb_app.run(host="0.0.0.0", port=8008)
     #
-    
+   
